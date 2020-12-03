4e2af10c610c6074f7ba8af82dd773b8f16ef497
Gary Collins <63056225+gscollins1973@users.noreply.github.com>
2020-12-03 15:09:45 +0000

    Update Logistic regression_validation_sample_size_parallel.R

--- Logistic regression_validation_sample_size_parallel.R
+++ Logistic regression_validation_sample_size_parallel.R
@@ -1,9 +1,12 @@
 # simulation-based approach to calculate the sample size required for 
 # external validation of a particular prediction model for a binary outcome
+# coded in parallel
+# 02-December-2020
+
 library(doParallel)
 library(foreach)
 library(rms)
-rm(list=ls())
+rm(list = ls())
 set.seed(200520)
 
 ## needs the rms package to be installed 
@@ -23,13 +26,12 @@ hanley_se_auc <- function(x, y){
   n2 <- sum(y == 1)
   Q1 <- x / (2 - x)
   Q2 <- 2 * x^2 / (1 + x)
-  sqrt((x*(1-x) + (n1-1)*(Q1-x^2)+(n2-1)*(Q2-x^2)) / (n1*n2))
+  sqrt((x * (1 - x) + (n1 - 1) * (Q1 - x^2) + (n2 - 1) * (Q2 - x^2)) / (n1 * n2))
 }
 
 my_fun <- function(){    
   # generate the linear predictor
   LP <- rnorm(n[k], mu.v, sd.v[j])
-  
   pr <- 1 / (1 + exp(-LP))
   
   # generate the outcome
@@ -38,7 +40,8 @@ my_fun <- function(){
   df <- data.frame(y = y, LP = LP)
   
   # fit calibration model
-  fit <- lrm(y~LP, data = df, x = T, y=T)
+  fit <- lrm(y~LP, data = df, x = T, y = T)
+  
   # get the c-statistic, its standard error and 95% CI width
   c.statistic    <- as.numeric(fit$stats['C'])
   c.statistic.se <- hanley_se_auc(c.statistic, y)
@@ -57,30 +60,30 @@ my_fun <- function(){
 }
 
 n_cores <- detectCores()
-cl <- makeCluster(n_cores - 1)
+cl      <- makeCluster(n_cores - 1)
 registerDoParallel(cl)
-system.time(tmp <- foreach(k = 1:length(n), .combine='cbind') %:% foreach(j = 1:length(sd.v), .combine = 'cbind') %:% foreach(i=1:NSIM, .packages='rms', .combine = 'rbind') %dopar% {rslt <- my_fun()})
+system.time(tmp <- foreach(k = 1:length(n), .combine = 'cbind') %:% foreach(j = 1:length(sd.v), .combine = 'cbind') %:% foreach(i = 1:NSIM, .packages = 'rms', .combine = 'rbind') %dopar% {rslt <- my_fun()})
 stopCluster(cl)
 OUT <- matrix(colMeans(matrix(unlist(tmp), ncol = 7 * 55, byrow = F)), ncol = 7,byrow = T)
 
 par(mfrow = c(1, 2))
-plot(OUT[c(1, 6, 11, 16, 21, 26, 31, 36, 41, 46, 51), 3],      OUT[c(1, 6, 11, 16, 21, 26, 31, 36, 41, 46, 51), 5],     type = 'b', xlab = 'Average number of events', ylab = 'Average 95% CI width for the C-statistic', ylim=c(0, max(OUT[,5])), xlim = c(0, max(OUT[,3])), pch = 4, cex.lab=0.75, xaxt = 'n')
-axis(side = 1, at = E[-1], cex.axis=0.75)
-lines(OUT[c(1, 6, 11, 16, 21, 26, 31, 36, 41, 46, 51) + 1, 3], OUT[c(1, 6, 11, 16, 21, 26, 31, 36, 41, 46, 51) + 1, 5], type = 'b', col = 2, cex = 0.5)
-lines(OUT[c(1, 6, 11, 16, 21, 26, 31, 36, 41, 46, 51) + 2, 3], OUT[c(1, 6, 11, 16, 21, 26, 31, 36, 41, 46, 51) + 2, 5], type = 'b', col = 3, cex = 0.5)
-lines(OUT[c(1, 6, 11, 16, 21, 26, 31, 36, 41, 46, 51) + 3, 3], OUT[c(1, 6, 11, 16, 21, 26, 31, 36, 41, 46, 51) + 3, 5], type = 'b', col = 4, cex = 0.5)
-lines(OUT[c(1, 6, 11, 16, 21, 26, 31, 36, 41, 46, 51) + 4, 3], OUT[c(1, 6, 11, 16, 21, 26, 31, 36, 41, 46, 51) + 4, 5], type = 'b', col = 5, cex = 0.5)
+plot(OUT[c(1, 6, 11, 16, 21, 26, 31, 36, 41, 46, 51), 3], OUT[c(1, 6, 11, 16, 21, 26, 31, 36, 41, 46, 51), 5], type = 'b', xlab = 'Average number of events', ylab = 'Average 95% CI width for the C-statistic', ylim = c(0, max(OUT[, 5])), xlim = c(0, max(OUT[, 3])), pch = 4, cex.lab = 0.75, xaxt = 'n')
+axis(side = 1, at = E[-1], cex.axis = 0.75)
+lines(OUT[c(1, 6, 11, 16, 21, 26, 31, 36, 41, 46, 51) + 1, 3], OUT[c(1, 6, 11, 16, 21, 26, 31, 36, 41, 46, 51) + 1, 5], type = 'b', col = 2, cex = 0.5, pch = 4)
+lines(OUT[c(1, 6, 11, 16, 21, 26, 31, 36, 41, 46, 51) + 2, 3], OUT[c(1, 6, 11, 16, 21, 26, 31, 36, 41, 46, 51) + 2, 5], type = 'b', col = 3, cex = 0.5, pch = 4)
+lines(OUT[c(1, 6, 11, 16, 21, 26, 31, 36, 41, 46, 51) + 3, 3], OUT[c(1, 6, 11, 16, 21, 26, 31, 36, 41, 46, 51) + 3, 5], type = 'b', col = 4, cex = 0.5, pch = 4)
+lines(OUT[c(1, 6, 11, 16, 21, 26, 31, 36, 41, 46, 51) + 4, 3], OUT[c(1, 6, 11, 16, 21, 26, 31, 36, 41, 46, 51) + 4, 5], type = 'b', col = 5, cex = 0.5, pch = 4)
 grid()
 legend("topright", col = 1:6, legend = as.character(sd.v), lty = 1, title = expression(paste('SD(LP) ', sigma)), cex = 0.75)
 abline(v = 100, lty = 2)
 abline(v = 200, lty = 2)
 
-plot(OUT[c(1, 6, 11, 16, 21, 26, 31, 36, 41, 46, 51), 3],    OUT[c(1, 6, 11, 16, 21, 26, 31, 36, 41, 46, 51), 7],       type = 'b', xlab = 'Average number of events', ylab = 'Average 95% CI width for the calibration slope', ylim=c(0, max(OUT[,7])), xlim = c(0, max(OUT[,3])), pch = 4, cex.lab=0.75, xaxt = 'n')
-axis(side = 1, at = E[-1], cex.axis=0.75)
-lines(OUT[c(1, 6, 11, 16, 21, 26, 31, 36, 41, 46, 51) + 1, 3], OUT[c(1, 6, 11, 16, 21, 26, 31, 36, 41, 46, 51) + 1, 7], type = 'b', col = 2, cex = 0.5)
-lines(OUT[c(1, 6, 11, 16, 21, 26, 31, 36, 41, 46, 51) + 2, 3], OUT[c(1, 6, 11, 16, 21, 26, 31, 36, 41, 46, 51) + 2, 7], type = 'b', col = 3, cex = 0.5)
-lines(OUT[c(1, 6, 11, 16, 21, 26, 31, 36, 41, 46, 51) + 3, 3], OUT[c(1, 6, 11, 16, 21, 26, 31, 36, 41, 46, 51) + 3, 7], type = 'b', col = 4, cex = 0.5)
-lines(OUT[c(1, 6, 11, 16, 21, 26, 31, 36, 41, 46, 51) + 4, 3], OUT[c(1, 6, 11, 16, 21, 26, 31, 36, 41, 46, 51) + 4, 7], type = 'b', col = 5, cex = 0.5)
+plot(OUT[c(1, 6, 11, 16, 21, 26, 31, 36, 41, 46, 51), 3], OUT[c(1, 6, 11, 16, 21, 26, 31, 36, 41, 46, 51), 7], type = 'b', xlab = 'Average number of events', ylab = 'Average 95% CI width for the calibration slope', ylim = c(0, max(OUT[, 7])), xlim = c(0, max(OUT[, 3])), pch = 4, cex.lab = 0.75, xaxt = 'n')
+axis(side = 1, at = E[-1], cex.axis = 0.75)
+lines(OUT[c(1, 6, 11, 16, 21, 26, 31, 36, 41, 46, 51) + 1, 3], OUT[c(1, 6, 11, 16, 21, 26, 31, 36, 41, 46, 51) + 1, 7], type = 'b', col = 2, cex = 0.5, pch = 4)
+lines(OUT[c(1, 6, 11, 16, 21, 26, 31, 36, 41, 46, 51) + 2, 3], OUT[c(1, 6, 11, 16, 21, 26, 31, 36, 41, 46, 51) + 2, 7], type = 'b', col = 3, cex = 0.5, pch = 4)
+lines(OUT[c(1, 6, 11, 16, 21, 26, 31, 36, 41, 46, 51) + 3, 3], OUT[c(1, 6, 11, 16, 21, 26, 31, 36, 41, 46, 51) + 3, 7], type = 'b', col = 4, cex = 0.5, pch = 4)
+lines(OUT[c(1, 6, 11, 16, 21, 26, 31, 36, 41, 46, 51) + 4, 3], OUT[c(1, 6, 11, 16, 21, 26, 31, 36, 41, 46, 51) + 4, 7], type = 'b', col = 5, cex = 0.5, pch = 4)
 grid()
 legend("topright", col = 1:6, legend = as.character(sd.v), lty = 1, title = expression(paste('SD(LP) ', sigma)), cex = 0.75)
 abline(v = 100, lty = 2)
